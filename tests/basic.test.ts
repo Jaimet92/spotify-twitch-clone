8a59653e997bd2b0e7881e669569f04d51e08733
jaimet92 <jaimetobalina92@gmail.com>
2023-10-05 23:11:21 +0200

    fix test

--- tests/basic.test.ts
+++ tests/basic.test.ts
@@ -3,7 +3,7 @@ import { expect, test } from "bun:test";
 // Edit an assertion and save to see HMR in action
 
 test('Math.sqrt()', () => {
-    expect(Math.sqrt(4)).toBe(4);
+    expect(Math.sqrt(4)).toBe(2);
     expect(Math.sqrt(144)).toBe(12);
     expect(Math.sqrt(2)).toBe(Math.SQRT2);
 });
